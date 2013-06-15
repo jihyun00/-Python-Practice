aad416117c1b31027752ce7108ddbd42ea66d725
jihyun <noblea1117@gmail.com>
2013-06-16 08:55:44 +0900

    bubble sort completed

--- bublble_sort/bubble.py
+++ bublble_sort/bubble.py
@@ -2,7 +2,6 @@ def bubble():
     num = []
     a = 0
     y = 0
-    k = 0
     count = input('input number which input in list')
     
     while y < count:
@@ -10,12 +9,14 @@ def bubble():
         num.append(x)
 	y = y+1
 
-    for i in range(count):
-	if i+1 < count:
-            if num[i] > num[i+1]:
-	        temp = num[i+1]
-		num[i+1] = num[i]
-		num[i] = temp
+    while count>0:
+        for i in range(count):
+	    if i+1 < count:
+                if num[i] > num[i+1]:
+	            temp = num[i+1]
+		    num[i+1] = num[i]
+		    num[i] = temp
+        count = count-1
 
     print num
 
